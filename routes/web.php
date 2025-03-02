54d6cb25d33d3ced422192fdda9ec38047270e6d
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-03-02 13:17:00 -0300

    refactor: Reorganize routes

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/jogos', [AppController::class, 'index']);
+Route::prefix('jogos')->group(function () {
+    Route::get('/', [AppController::class, 'index'])->name('jogos.index');
+});
+
+Route::fallback(function () {
+    return 'Página não encontrada';
+});
