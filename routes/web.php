a65efc0f19dacff4927409a8e56fa3d75bcff679
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-03-03 19:11:09 -0300

    feat: Init page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('jogos.index');
 });
 
 Route::prefix('jogos')->group(function () {
